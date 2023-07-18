591c53d9f96c24cb9a97e19a7c8a7e1c4ac29c24
igorosors <igorosors@yandex.ru>
2023-07-18 21:47:21 +0400

    ain

--- Profnotes/app/src/main/java/com/example/profnotes/MainActivity.kt
+++ Profnotes/app/src/main/java/com/example/profnotes/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.profnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
